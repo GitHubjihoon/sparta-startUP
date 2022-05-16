bb7f37decd24acf1a480c38c314f8fd78ee0b366
KimJihoon <jihoon951118@gmail.com>
2022-05-16 22:30:52 +0900

    bs4 select_one

--- pythonprac/hello.py
+++ pythonprac/hello.py
@@ -1,1 +1,13 @@
-print('hello starta!!')
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.naver?sel=pnt&date=20210829',headers=headers)
+
+soup = BeautifulSoup(data.text, 'html.parser')
+
+title = soup.select_one('#old_content > table > tbody > tr:nth-child(2) > td.title > div > a')
+
+print(title)
+print(title.text)
+print(title['href'])
